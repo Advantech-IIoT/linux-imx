60f4ffe4bd5dd85148cf94dd1a0ebc078ddda145
john99.chang <john99.chang@advantech.com.tw>
2026-09-17 16:18:05 +0800

    redmine #50850 fix gpios_export.c error

--- drivers/misc/gpios_export.c
+++ drivers/misc/gpios_export.c
@@ -1,16 +1,18 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/of_platform.h>
-#include <linux/of_gpio.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/gpio/consumer.h>
+#include <linux/gpio.h>
 
 static int gpio_export_probe(struct platform_device *pdev)
 {
 	char gpio_name[32]={0};
 	struct device_node *np = pdev->dev.of_node;
-	enum of_gpio_flags gpio_flag;	
+	struct gpio_desc *desc;
 	u32 count, i;
-	int flags;
-	int export_gpio;
+	unsigned int export_gpio;
+	bool output;
 	int ret = 0;
 
 	ret = of_property_read_u32(np, "gpio_counts", &count);
@@ -26,40 +28,110 @@ static int gpio_export_probe(struct platform_device *pdev)
 			u32 gpio_prop[2];
 
 			ret = of_property_read_u32_array(np, gpio_name, gpio_prop, 2);
-   			if (ret < 0) {
-				dev_warn(&pdev->dev, "invalid %s \n", gpio_name);
-				export_gpio = -1;
-			}else {
-				export_gpio = gpio_prop[0];
-				gpio_flag  = gpio_prop[1];
-			}
-		}else {
-			export_gpio = of_get_named_gpio_flags(np, gpio_name, 0, &gpio_flag);
-		}
-
-		if (gpio_is_valid(export_gpio)) {
-			if(gpio_flag)
-				flags = GPIOF_DIR_OUT;
-			else
-				flags = GPIOF_DIR_IN;
-
-			ret = devm_gpio_request_one(&pdev->dev,
-					export_gpio, flags, gpio_name);
 			if (ret) {
-				dev_err(&pdev->dev, "unable to get gpio %s\n", gpio_name);
+				dev_err(&pdev->dev, "invalid %s property\n", gpio_name);
 				return ret;
 			}
 
-			//export the gpio.
-			gpio_export(export_gpio, 1);
-		}else{
-			if (export_gpio == PTR_ERR(-EPROBE_DEFER)) {
-				return PTR_ERR(-EPROBE_DEFER);
+			export_gpio = gpio_prop[0];
+			output = !!gpio_prop[1];
+
+			desc = gpio_to_desc(export_gpio);
+			if (!desc) {
+				dev_err(&pdev->dev,
+					"%s: invalid GPIO %u\n",
+					gpio_name, export_gpio);
+				return -EINVAL;
 			}
 
-			dev_err(&pdev->dev, "%s is invalid\n", gpio_name);
-			return -1;
+			/*
+			 * Request GPIO using descriptor API.
+			 */
+			ret = devm_gpio_request(&pdev->dev,
+						export_gpio, gpio_name);
+			if (ret) {
+				dev_err(&pdev->dev,
+					"unable to request GPIO %u (%s): %d\n",
+					export_gpio, gpio_name, ret);
+				return ret;
+			}
+
+			/*
+			 * Set direction.
+			 */
+			if (output)
+				ret = gpiod_direction_output(desc, 0);
+			else
+				ret = gpiod_direction_input(desc);
+
+			if (ret) {
+				dev_err(&pdev->dev,
+					"unable to set direction for GPIO %u: %d\n",
+					export_gpio, ret);
+				return ret;
+			}
+		}else {
+			desc = devm_fwnode_gpiod_get_index(
+				&pdev->dev,
+				of_fwnode_handle(np),
+				gpio_name,
+				0,
+				GPIOD_ASIS,
+				gpio_name);
+
+			if (IS_ERR(desc)) {
+				ret = PTR_ERR(desc);
+				dev_err(&pdev->dev,
+					"unable to get %s: %d\n",
+					gpio_name, ret);
+				return ret;
+			}
+
+			export_gpio = desc_to_gpio(desc);
+
+			ret = gpiod_get_direction(desc);
+			if (ret < 0) {
+				dev_err(&pdev->dev,
+					"unable to get direction of %s: %d\n",
+					gpio_name, ret);
+				return ret;
+			}
+
+			if (ret == 0)
+				output = true;
+			else
+				output = false;
+
+			if (output)
+				ret = gpiod_direction_output(desc, 0);
+			else
+				ret = gpiod_direction_input(desc);
+
+			if (ret) {
+				dev_err(&pdev->dev,
+					"unable to set direction of %s: %d\n",
+					gpio_name, ret);
+				return ret;
+			}
 		}
+
+		/*
+		 * Export GPIO to /sys/class/gpio/
+		 *
+		 * direction_may_change = true
+		 */
+		ret = gpiod_export(desc, true);
+		if (ret) {
+			dev_err(&pdev->dev,
+				"unable to export %s: %d\n",
+				gpio_name, ret);
+			return ret;
+		}
+		dev_info(&pdev->dev,
+			 "exported %s (GPIO %u, %s)\n",
+			 gpio_name,
+			 export_gpio,
+			 output ? "output" : "input");
 	}
 
 	return 0;
